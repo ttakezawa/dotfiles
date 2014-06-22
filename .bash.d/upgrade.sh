116476a0458d1692ded57ddb18421d3367759373
ttakezawa <ttakezawa@users.noreply.github.com>
2014-06-22 14:46:34 +0900

    bash: modify .bash.d/upgrade.sh

--- .bash.d/upgrade.sh
+++ .bash.d/upgrade.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# -*- coding:utf-8; mode:sh; sh-basic-offset:2; sh-indentation:2; -*-
+
 SOURCE="${BASH_SOURCE[0]}"
 dir="$( builtin cd -P "$( dirname "$SOURCE" )" && builtin pwd )"
 cd $dir
@@ -6,4 +8,9 @@ cd $dir
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > $dir/git-completion.bash
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > $dir/git-prompt.sh
 
-(cd completion-ruby && git checkout master && git pull)
+( # update completion-ruby
+  git submodule update --init completion-ruby &&
+  cd completion-ruby &&
+  git checkout master &&
+  git pull
+)
