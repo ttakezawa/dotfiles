25cc0ab30c359a6893b1c5862dfb494443375a1f
ttakezawa <ttakezawa@users.noreply.github.com>
2014-06-26 23:08:02 +0900

    emacs: Add bin/make_filelist.sh

---
+++ bin/make_filelist.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+dir=$HOME/.emacs.d
+lockfile=$dir/filelist.lock
+
+home_filelist=$dir/home.filelist
+system_filelist=$dir/system.filelist
+
+filelock ()   { exec 9>>$1; flock -w 0 -x 9; }
+fileunlock () { exec 9>&-; }
+
+if ! filelock $lockfile; then
+    echo "$(date "+%Y/%m/%d %H:%M:%S%t")Failed to get lock"
+    exit 1
+fi
+
+ignore='(\.lo|\.o|\.elc|\.jpg|\.png|\.gif|\.swf|~|#)$'
+
+rm -f $home_filelist.new
+locate --regex "^$HOME" | grep -Pv $ignore >> $home_filelist.new
+if [[ -s $home_filelist.new ]]; then
+    mv $home_filelist.new $home_filelist
+    echo "$(date "+%Y/%m/%d %H:%M:%S%t")updated $home_filelist"
+fi
+
+rm -f $system_filelist.new
+locate --regex '^/(bin|boot|etc|lib|opt|sbin|usr|var)' | grep -Pv $ignore >> $system_filelist.new
+if [[ -s $system_filelist.new ]]; then
+    mv $system_filelist.new $system_filelist
+    echo "$(date "+%Y/%m/%d %H:%M:%S%t")updated $system_filelist"
+fi
+
+fileunlock
