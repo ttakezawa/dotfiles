5b87850cc8311e97df4f26352ea70bd50cab08c6
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2014-07-13 09:48:56 +0900

    emacs: Added key-bind M-.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -124,6 +124,22 @@
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 (define-key helm-read-file-map (kbd "C-z") 'helm-select-action)
 (define-key helm-find-files-map (kbd "C-z") 'helm-select-action)
+;; via: http://d.hatena.ne.jp/syohex/20131016/1381935863
+(global-set-key (kbd "M-.") 'takezawa/helm-etags-select)
+(defun takezawa/helm-etags-select (arg)
+  (interactive "P")
+  (let ((tag  (helm-etags-get-tag-file))
+        (helm-execute-action-at-once-if-one t))
+    (when (or (equal arg '(4))
+              (and helm-etags-mtime-alist
+                   (helm-etags-file-modified-p tag)))
+      (remhash tag helm-etags-cache))
+    (if (and tag (file-exists-p tag))
+        (helm :sources 'helm-source-etags-select :keymap helm-etags-map
+              :input (concat (thing-at-point 'symbol) " ")
+              :buffer "*helm etags*"
+              :default (concat "\\_<" (thing-at-point 'symbol) "\\_>"))
+      (message "Error: No tag file found, please create one with etags shell command."))))
 
 ;;;; {helm-ag}
 (global-set-key (kbd "C-c g") 'helm-ag)
