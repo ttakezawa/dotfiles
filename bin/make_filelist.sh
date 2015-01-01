73b4271d023b5c5adea161cb3b77d3c338831d9e
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2015-01-02 02:21:16 +0900

    Update bin/make_filelist.sh

--- bin/make_filelist.sh
+++ bin/make_filelist.sh
@@ -15,16 +15,17 @@ if ! filelock $lockfile; then
 fi
 
 ignore='(\.lo|\.o|\.elc|\.jpg|\.png|\.gif|\.swf|~|#)$'
+ignore_dir='/([^/]+\.old|cache)/'
 
 rm -f $home_filelist.new
-locate --regex "^$HOME" | grep -Pv $ignore >> $home_filelist.new
+locate --regex "^$HOME" | grep -Pv $ignore_dir | grep -Pv $ignore >> $home_filelist.new
 if [[ -s $home_filelist.new ]]; then
     mv $home_filelist.new $home_filelist
     echo "$(date "+%Y/%m/%d %H:%M:%S%t")updated $home_filelist"
 fi
 
 rm -f $system_filelist.new
-locate --regex '^/(bin|boot|etc|lib|opt|sbin|usr|var)' | grep -Pv $ignore >> $system_filelist.new
+locate --regex '^/(bin|boot|etc|lib|opt|sbin|usr|var)' | grep -Pv $ignore_dir | grep -Pv $ignore >> $system_filelist.new
 if [[ -s $system_filelist.new ]]; then
     mv $system_filelist.new $system_filelist
     echo "$(date "+%Y/%m/%d %H:%M:%S%t")updated $system_filelist"
