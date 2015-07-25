c76617a09db3ffc00bcbdff6ab49363ee6540889
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2015-07-26 01:39:13 +0900

    bash: Update install.sh to install ghq binary

--- install.sh
+++ install.sh
@@ -8,13 +8,26 @@ if [[ "$(uname -m)" =~ 64 ]]; then
   IS_64=1
 fi
 
-# Install peco from https://github.com/peco/peco/releases
-PECO_VERSION=v0.3.3
 if [[ $IS_64 ]]; then
   PROCESSOR=amd64
 else
   PROCESSOR=386
 fi
+
+# Install ghq from https://github.com/motemen/ghq/releases
+GHQ_VERSION=v0.6
+echo Install ghq $GHQ_VERSION
+if [[ "$OSTYPE" =~ linux ]]; then
+  curl -L https://github.com/motemen/ghq/releases/download/${GHQ_VERSION}/ghq_linux_${PROCESSOR}.tar.gz \
+    | tar xz -C $dir/bin --strip=1 --wildcards '*/ghq' --no-same-owner --no-same-permissions
+elif [[ "$OSTYPE" =~ darwin ]]; then
+  curl -L https://github.com/motemen/ghq/releases/download/${GHQ_VERSION}/ghq_darwin_${PROCESSOR}.zip \
+    | bsdtar -xzf - -C $dir/bin --strip=1 '*/ghq'
+fi
+
+# Install peco from https://github.com/peco/peco/releases
+PECO_VERSION=v0.3.3
+echo Install peco $PECO_VERSION
 if [[ "$OSTYPE" =~ linux ]]; then
   curl -L https://github.com/peco/peco/releases/download/${PECO_VERSION}/peco_linux_${PROCESSOR}.tar.gz \
     | tar xz -C $dir/bin --strip=1 --wildcards '*/peco' --no-same-owner --no-same-permissions
