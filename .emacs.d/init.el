7ad0144a4774f41be581560da8dcdfa82b80cf5b
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2017-01-24 02:59:27 +0900

    emacs: Use pbpaste

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -101,6 +101,20 @@
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
 
+;; macOSのクリップボードを活用させる ( Taken from https://gist.github.com/the-kenny/267162 )
+(when (eq system-type 'darwin)
+  (defun copy-from-osx ()
+    (shell-command-to-string "pbpaste"))
+
+  (defun paste-to-osx (text &optional push)
+    (let ((process-connection-type nil))
+      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+        (process-send-string proc text)
+        (process-send-eof proc))))
+
+  (setq interprogram-cut-function 'paste-to-osx)
+  (setq interprogram-paste-function 'copy-from-osx))
+
 ;; ダブルクリックでURLを開く
 (global-set-key [double-mouse-1] 'ffap-at-mouse)
 
