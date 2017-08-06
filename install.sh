7908294878741eb5f421fe497f3139da24b4a623
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2017-08-06 22:34:04 +0900

    bash: Modify install.sh to install manpage of exa

--- install.sh
+++ install.sh
@@ -14,14 +14,6 @@ else
   PROCESSOR=386
 fi
 
-# Install ghq from https://github.com/motemen/ghq/releases
-GHQ_VERSION=v0.7.2
-echo Install ghq $GHQ_VERSION
-if [[ "$OSTYPE" =~ linux ]]; then
-  curl -L https://github.com/motemen/ghq/releases/download/${GHQ_VERSION}/ghq_linux_${PROCESSOR}.zip \
-    | bsdtar -xzf - -C $dir/bin --strip=0 './ghq'
-elif [[ "$OSTYPE" =~ darwin ]]; then
-  curl -L https://github.com/motemen/ghq/releases/download/${GHQ_VERSION}/ghq_darwin_${PROCESSOR}.zip \
-    | bsdtar -xzf - -C $dir/bin --strip=0 './ghq'
-fi
-chmod +x $dir/bin/ghq
+# Install exa manpage
+mkdir -p $HOME/local/share/man/man1
+curl -L https://raw.githubusercontent.com/ogham/exa/master/contrib/man/exa.1 -o $HOME/local/share/man/man1/exa.1
