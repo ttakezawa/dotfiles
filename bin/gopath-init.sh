62098e2b51894a446ab7e3c124fb5540518172e6
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2017-08-18 09:58:38 +0900

    Add bin/gopath-init.sh

---
+++ bin/gopath-init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ -d $PWD/.gopath ]]; then
+  echo $PWD/.gopath already exists.
+  exit 1
+fi
+
+if [[ -f $PWD/.envrc ]]; then
+  echo $PWD/.envrc already exists.
+  exit 1
+fi
+
+pkg=${PWD##"$HOME/dev/src/"}
+pkgdir=$PWD/.gopath/src/$pkg
+
+cat <<'EOF' > $PWD/.envrc
+export GOPATH=$PWD/.gopath
+PATH_add $GOPATH/bin
+EOF
+
+direnv allow
+
+mkdir -p $(dirname $pkgdir)
+ln -sfv $PWD $pkgdir
