1d6aeda8d34b1f7f177b7d6f2109d62023c9985a
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2017-08-18 09:33:14 +0900

    Add bin/ec2-hosts.sh

---
+++ bin/ec2-hosts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Example Output:
+# web01  2017-07-29T08:51:32.000Z  i-02fd145fecb87ccb3  10.10.1.11  running
+# web02  2017-07-29T09:19:02.000Z  i-094f827affc84a7a2  10.10.1.12  running
+
+AWS_DEFAULT_REGION=ap-northeast-1 aws ec2 describe-instances | jq -r '.Reservations | .[].Instances[] | [(.Tags[] | select(.Key == "Name").Value), .LaunchTime, .InstanceId, .PrivateIpAddress, .State.Name] | @tsv' | sort | column -t
