ad0ae7b6d5265154ca96e2ece585e93874a9100b
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2017-10-22 12:18:16 +0900

    bash: Add diff-so-fancy to upgrade-dependencyies

--- upgrade-dependencies.sh
+++ upgrade-dependencies.sh
@@ -41,3 +41,6 @@ echo '#!/usr/bin/perl' > bin/diff-highlight
 curl -L https://raw.githubusercontent.com/git/git/master/contrib/diff-highlight/DiffHighlight.pm >> bin/diff-highlight
 curl -L https://raw.githubusercontent.com/git/git/master/contrib/diff-highlight/diff-highlight.perl >> bin/diff-highlight
 chmod +x bin/diff-highlight
+
+curl -L https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy -o bin/diff-so-fancy
+chmod +x bin/diff-so-fancy
