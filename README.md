67d53c9d0df9b8208f2c12ad87355402d327be75
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2019-03-24 16:22:20 +0900

    Refator README.md and install.sh

--- README.md
+++ README.md
@@ -1,35 +1,15 @@
 ## Setup
 
 ```bash
+# installation
 mkdir -p ~/go/src/github.com/ttakezawa
-git clone git@github.com:ttakezawa/dotfiles.git ~/go/src/github.com/ttakezawa/dotfiles
-ln -s ~/go/src/github.com/ttakezawa/dotfiles ~/.dotfiles
+git clone -p git@github.com:ttakezawa/dotfiles.git ~/go/src/github.com/ttakezawa/dotfiles
+ln -nfs ~/go/src/github.com/ttakezawa/dotfiles ~/.dotfiles
 
-# Mac OS X
-echo 'source $HOME/.dotfiles/.bashrc' >> ~/.bash_profile
+~/.dotfiles/install.sh
 
-# Linux
-echo 'source ~/.dotfiles/.bashrc' >> ~/.bashrc
-
-# Common
-ln -s ~/.dotfiles/.ssh_config ~/.ssh/config
-ln -s ~/.dotfiles/.inputrc ~/
-ln -s ~/.dotfiles/.gitconfig ~/
-ln -s ~/.dotfiles/.gitignore_global ~/
-ln -s ~/.dotfiles/.gitattributes_global ~/
-ln -s ~/.dotfiles/.byobu-tmux.conf ~/
-mkdir -p ~/.byobu; echo 'source $HOME/.byobu-tmux.conf' >> ~/.byobu/.tmux.conf
-ln -s ~/.dotfiles/.tmux.conf ~/
-ln -s ~/.dotfiles/.emacs.d ~/
-ln -s ~/.dotfiles/.vimrc ~/
-ln -s ~/.dotfiles/.cobra.yaml ~/
-ln -s ~/.dotfiles/.agignore ~/
-ln -s ~/.dotfiles/.aspell.conf ~/.dotfiles/.aspell.en.pws ~/
-ln -s ~/.dotfiles/.gemrc ~/
+# misc
 git config --global pr-release.template .pr-release.erb
 git config --global pr-release.branch.staging development
 # ln -s ~/.dotfiles/.pr-release.erb ./
-
-mkdir -p ~/.atom
-ln -s ~/.dotfiles/.atom/keymap.cson ~/.atom/
 ```
--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 SOURCE="${BASH_SOURCE[0]}"
 dir="$( builtin cd -P "$( dirname "$SOURCE" )" && builtin pwd )"
 cd $dir
@@ -13,6 +15,48 @@ if [[ $IS_64 ]]; then
 else
   PROCESSOR=386
 fi
+
+case $OSTYPE in
+  darwin*) IS_DARWIN=1 ;;
+  linux*)  IS_LINUX=1  ;;
+esac
+
+# Linux
+if [[ $IS_LINUX ]]; then
+  if ! grep -qs .dotfiles/.bashrc ~/.bashrc; then
+    echo 'source $HOME/.dotfiles/.bashrc' >> ~/.bashrc
+  fi
+fi
+
+# macOS
+if [[ $IS_DARWIN ]]; then
+  if ! grep -qs .dotfiles/.bashrc ~/.bash_profile; then
+    echo 'source $HOME/.dotfiles/.bashrc' >> ~/.bash_profile
+  fi
+fi
+
+# Common
+ln -fs  ~/.dotfiles/.ssh_config ~/.ssh/config
+ln -fs  ~/.dotfiles/.inputrc ~/
+ln -fs  ~/.dotfiles/.gitconfig ~/
+ln -fs  ~/.dotfiles/.gitignore_global ~/
+ln -fs  ~/.dotfiles/.gitattributes_global ~/
+ln -fs  ~/.dotfiles/.tmux.conf ~/
+ln -nfs ~/.dotfiles/.emacs.d ~/
+ln -fs  ~/.dotfiles/.vimrc ~/
+ln -fs  ~/.dotfiles/.cobra.yaml ~/
+ln -fs  ~/.dotfiles/.agignore ~/
+ln -fs  ~/.dotfiles/.aspell.conf ~/.dotfiles/.aspell.en.pws ~/
+ln -fs  ~/.dotfiles/.gemrc ~/
+
+ln -fs ~/.dotfiles/.byobu-tmux.conf ~/
+mkdir -p ~/.byobu
+if ! grep -qs byobu-tmux.conf ~/.byobu/.tmux.conf; then
+  echo 'source $HOME/.byobu-tmux.conf' >> ~/.byobu/.tmux.conf
+fi
+
+mkdir -p ~/.atom
+ln -fs ~/.dotfiles/.atom/keymap.cson ~/.atom/
 
 if type -P sshrc >/dev/null; then
   # setup sshrc
