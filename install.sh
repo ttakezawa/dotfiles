8fb4957e6fe591aba81044a84d4b3dbdef34c13c
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2021-02-05 23:24:38 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@ set -euo pipefail
 
 install() {
   mkdir -p ~/go/src/github.com/ttakezawa
-  git clone git@github.com:ttakezawa/dotfiles.git ~/go/src/github.com/ttakezawa/dotfiles
+  if [[ ! -d ~/go/src/github.com/ttakezawa/dotfiles/.git ]]; then
+    git clone git@github.com:ttakezawa/dotfiles.git ~/go/src/github.com/ttakezawa/dotfiles
+  fi
   ln -nfs ~/go/src/github.com/ttakezawa/dotfiles ~/.dotfiles
 
   ~/.dotfiles/deploy.sh
