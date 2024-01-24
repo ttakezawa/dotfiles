a71636a5dc21ec6c91c9a8c25f488b1a60c0b287
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2024-01-24 17:02:22 +0900

    Add mappings for Raycast and deploy Karabiner modifications

--- karabiner/deploy.sh
+++ karabiner/deploy.sh
@@ -6,5 +6,5 @@ while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 SOURCE_DIR="$(cd -P "$( dirname "$SOURCE" )" && pwd)"
 
 mkdir -p ~/.config/karabiner/assets/complex_modifications
-ln -fs "$SOURCE_DIR/MyMappings.json"     ~/.config/karabiner/assets/complex_modifications/
-ln -fs "$SOURCE_DIR/UnusedMappings.json" ~/.config/karabiner/assets/complex_modifications/
+ln -fs "$SOURCE_DIR"/*.json ~/.config/karabiner/assets/complex_modifications/
+
